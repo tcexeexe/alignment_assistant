e8f56a48c201581cc224be1914635cb490ed0e1c
tcexeexe <heji12345678@163.com>
2024-03-15 18:41:21 +0800

    修改说明

--- app.py
+++ app.py
@@ -53,6 +53,7 @@ iface = gr.Interface(
     outputs="text",  # 输出类型为文本
     title="大模型对齐小助手",
     description='''
+    基于internLM2-7b-sft训练，融合正能量数据集
     代替RLHF阶段的奖励模型，对LLM的输出进行打分。
     可以在两个输入框中分别输入问题和回答，点击提交查看回答评分结果。
     回答越优质，得分越高，得分小于0，则不合格。
